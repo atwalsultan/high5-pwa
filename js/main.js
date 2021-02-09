ae00a2e92756f52647d610969bf41ed90abc17c4
Sultan <sultan.singh.atwal@gmail.com>
2021-02-08 17:29:25 -0800

    Code clean up and maintenance

--- js/main.js
+++ js/main.js
@@ -10,10 +10,6 @@
 //**************************************************************
 // Create functionality
 const postForm = document.getElementById("postForm");
-const activityDate = document.getElementById("activityDate");
-const activityTime = document.getElementById("activityTime");
-const activityDesc = document.getElementById("activityDesc");
-const activityCategory = document.getElementById("activityCategory");
 const createBtn = document.getElementById('createBtn');
 const createOverlay = document.getElementById('createOverlay');
 
@@ -21,16 +17,12 @@ const createOverlay = document.getElementById('createOverlay');
 const postList = document.getElementById('post-list');
 
 // Update functionality
-let updateIndex;
+let updateId;
 const updateForm = document.getElementById('updateForm');
-const updateDate = document.getElementById('updateDate');
-const updateTime = document.getElementById('updateTime');
-const updateDesc = document.getElementById('updateDesc');
-const updateCategory = document.getElementById('updateCategory');
 const updateOverlay = document.getElementById('updateOverlay');
 
 // Delete functionality
-let deleteIndex;
+let deleteId;
 const confirmDelete = document.getElementById('confirmDelete');
 const deleteOverlay = document.getElementById('deleteOverlay');
 
@@ -55,12 +47,12 @@ const createPost = (event) => {
     let longitude;
 
     // Get values from DOM
-    activityDate.value === "" ? date = "Unspecified" : date = activityDate.value; // date = 'Unspedified' if user leaves it empty
-    activityTime.value === "" ? time = "Unspecified" : time = activityTime.value; // time = 'Unspedified' if user leaves it empty
-    let description = activityDesc.value;
-    let category = activityCategory.value;
-    userPos ? latitude = userPos.coords.latitude : latitude = ""; // latitude = '' if the user position is not available
-    userPos ? longitude = userPos.coords.longitude : longitude = ""; // longitude = '' if the user position is not available
+    postForm.activityDate.value === "" ? date = "Unspecified" : date = postForm.activityDate.value; // date = 'Unspedified' if user leaves it empty
+    postForm.activityTime.value === "" ? time = "Unspecified" : time = postForm.activityTime.value; // time = 'Unspedified' if user leaves it empty
+    let description = postForm.activityDesc.value;
+    let category = postForm.activityCategory.value;
+    userPos ? latitude = userPos.coords.latitude : latitude = 0; // latitude = '' if the user position is not available
+    userPos ? longitude = userPos.coords.longitude : longitude = 0; // longitude = '' if the user position is not available
 
     // Add post as document to collection
     db.collection('posts').add({
@@ -68,32 +60,31 @@ const createPost = (event) => {
         time: time,
         category: category,
         description: description,
-        latitude: latitude,
-        longitude: longitude
+        coordinates: new firebase.firestore.GeoPoint(latitude, longitude)
     });
 
-    // Clear inputs and close modal
+    // Clear form and close modal
     closeModals();
 }
 
 const addButtonListeners = (updateBtn, deleteBtn, doc) => {
     updateBtn.addEventListener('click', (event) => {
         // Get array index of post to update
-        updateIndex = event.target.parentElement.getAttribute('id'); //doc.id??
+        updateId = doc.id;
 
         // Show update form
         updateOverlay.style.display = 'block';
 
         // Populate form with existing post data
-        updateDate.value = doc.data().date;
-        updateTime.value = doc.data().time;
-        updateDesc.value = doc.data().description;
-        updateCategory.value = doc.data().category;
+        updateForm.updateDate.value = doc.data().date;
+        updateForm.updateTime.value = doc.data().time;
+        updateForm.updateDesc.value = doc.data().description;
+        updateForm.updateCategory.value = doc.data().category;
     })
 
     deleteBtn.addEventListener('click', (event) => {
         // Get array index of post to delete
-        deleteIndex = event.target.parentElement.getAttribute('id'); //doc.id??
+        deleteId = doc.id;
 
         // Show confirmation modal
         deleteOverlay.style.display = 'block';
@@ -145,17 +136,17 @@ const updatePost = (event) => {
     let updateObj = {};
 
     // Populating update object
-    updateDate.value === "" ? updateObj.date = 'Unspecified' : updateObj.date = updateDate.value; // date = 'Unspedified' if user leaves it empty
-    updateTime.value === "" ? updateObj.time = 'Unspecified' : updateObj.time = updateTime.value; // time = 'Unspedified' if user leaves it empty
-    updateObj.category = updateCategory.value;
-    updateObj.description = updateDesc.value;
+    updateForm.updateDate.value === "" ? updateObj.date = 'Unspecified' : updateObj.date = updateForm.updateDate.value; // date = 'Unspedified' if user leaves it empty
+    updateForm.updateTime.value === "" ? updateObj.time = 'Unspecified' : updateObj.time = updateForm.updateTime.value; // time = 'Unspedified' if user leaves it empty
+    updateObj.category = updateForm.updateCategory.value;
+    updateObj.description = updateForm.updateDesc.value;
     if(userPos) { // TODO: Add a checkbox in DOM to choose if users want to update the position or use the pre-existing position?
         updateObj.latitude = userPos.coords.latitude;
         updateObj.longitude = userPos.coords.longitude;
     }
 
     // Updating document in collection
-    db.collection('posts').doc(updateIndex).update(updateObj);
+    db.collection('posts').doc(updateId).update(updateObj);
 
     // Clear form and close modal
     closeModals();
@@ -164,7 +155,7 @@ const updatePost = (event) => {
 // Delete post
 const deletePost = () => {
     // Delete document from collection
-    db.collection('posts').doc(deleteIndex).delete();
+    db.collection('posts').doc(deleteId).delete();
 
     // Close modal
     closeModals();
@@ -173,15 +164,9 @@ const deletePost = () => {
 // Close modals
 const closeModals = () => {
 
-    // Clear inputs
-    activityDate.value = '';
-    activityTime.value = '';
-    activityDesc.value = '';
-    activityCategory.value = 'Category A';
-    updateDate.value = '';
-    updateTime.value = '';
-    updateDesc.value = '';
-    updateCategory.value = 'Category A';
+    // Clear forms
+    postForm.reset();
+    updateForm.reset();
 
     // Close modals
     createOverlay.style.display = 'none';
