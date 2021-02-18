0f5208e59a8d8b67c478c903a766e785218637bb
Sultan <sultan.singh.atwal@gmail.com>
2021-02-18 11:10:30 -0800

    Confirm password on sign up

--- js/index.js
+++ js/index.js
@@ -21,12 +21,25 @@ signupForm.addEventListener('submit', (e) => {
     // Get email and password from DOM
     let email = signupForm.signupEmail.value;
     let password = signupForm.signupPassword.value;
+    let confirmPassword = signupForm.confirmPassword.value;
 
-    // Sign up the user or show error message
-    auth.createUserWithEmailAndPassword(email, password).catch((error) => {
+    // Clear form
+    signupForm.reset();
+    signupForm.signupEmail.focus();
+
+    // Check if passwords match
+    if(password === confirmPassword) {
+        // Sign up the user or show error message
+        auth.createUserWithEmailAndPassword(email, password).catch((error) => {
+            // Show message
+            console.log(error.message);
+        });
+    }
+    else {
         // Show message
-        console.log(error.message);
-    });
+        console.log(`The passwords do not match. Please try again.`);
+    }
+
 });
 
 loginForm.addEventListener('submit', (e) => {
@@ -36,6 +49,10 @@ loginForm.addEventListener('submit', (e) => {
     // Get email and password from DOM
     let email = loginForm.loginEmail.value;
     let password = loginForm.loginPassword.value;
+
+    // Clear form
+    loginForm.reset();
+    loginForm.loginEmail.focus();
 
     // Sign up the user or show error message
     auth.signInWithEmailAndPassword(email, password).catch((error) => {
